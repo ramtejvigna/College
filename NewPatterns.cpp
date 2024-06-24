5c01518e1af176eff8b2edc1ac89eee36e369484
ramtejvigna <vignaramtejtelagarapu@gmail.com>
2024-06-24 14:35:23 +0530

    Added Rhombus function

--- NewPatterns.cpp
+++ NewPatterns.cpp
@@ -1,28 +1,68 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-void SquareDiagonal(int n) {
-    for(int i=0;i<n;i++) {
-        for(int j=0;j<n;j++) {
-            if(i == 0 || i == n-1 || j == 0 || j == n-1) {
+void SquareDiagonal(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (i == 0 || i == n - 1 || j == 0 || j == n - 1)
+            {
                 cout << "* ";
             }
-            else if(j == (n/2) || i == (n/2)) {
+            else if (j == (n / 2) || i == (n / 2))
+            {
                 cout << "* ";
             }
-            else if(i == j || i == (n-j)-1) {
+            else if (i == j || i == (n - j) - 1)
+            {
                 cout << "* ";
             }
-            else {
+            else
+            {
                 cout << "  ";
             }
         }
         cout << endl;
     }
 }
+void Rhombus(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n-i; j++)
+        {
+            cout << "  ";    
+        }
+        for(int j=0;j<=2*i;j++) {
+            if(j == 0 || j == 2*i || j == i || i == n-1) {
+                cout << "* ";
+            } else {
+                cout << "  ";
+            }
+        }
+        cout << endl;
+    }
 
-int main() {
-    int n; cin >> n;
+    for(int i=n;i>0;i--) {
+        for(int j=0;j<n-i+1;j++) {
+            cout << "  ";
+        }
+        for(int j=0;j<2*i;j++) {
+            if(j == 0 || j == i-1 || j == 2*i-2) 
+                cout << "* ";
+            else 
+                cout << "  ";
+        }
+        cout << endl;
+    }
+}
+int main()
+{
+    int n;
+    cin >> n;
 
-    SquareDiagonal(n);
+    // SquareDiagonal(n);
+    Rhombus(n);
 }
