6114ef50de08391fa4f8516d3a918f96c90215ec
ramtejvigna <vignaramtejtelagarapu@gmail.com>
2024-06-24 11:16:31 +0530

    Update NewPatterns.cpp

--- NewPatterns.cpp
+++ NewPatterns.cpp
@@ -4,11 +4,20 @@ using namespace std;
 void SquareDiagonal(int n) {
     for(int i=0;i<n;i++) {
         for(int j=0;j<n;j++) {
-            if(i == j) {
+            if(i == 0 || i == n-1 || j == 0 || j == n-1) {
                 cout << "* ";
             }
-            if()
+            else if(j == (n/2) || i == (n/2)) {
+                cout << "* ";
+            }
+            else if(i == j || i == (n-j)-1) {
+                cout << "* ";
+            }
+            else {
+                cout << "  ";
+            }
         }
+        cout << endl;
     }
 }
 
