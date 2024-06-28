70922d616acb0d2d5506afd0eb8de531b9f9c0a5
ramtejvigna <vignaramtejtelagarapu@gmail.com>
2024-06-28 19:47:16 +0530

    Optimized the code to O(1) time complexity

--- SwapBits.cpp
+++ SwapBits.cpp
@@ -2,13 +2,18 @@
 using namespace std;
 
 void optimize() {
+
     int n; cin >> n;
-   
+    int extVar = 255;  // extVar in binary - 11111111
 
-    int set1 = n << 4;cout<<n;
-    int set2 = n >> 4;cout<<n;
+    int set1 = n << 4;   // Integer has 32 bits space
+    set1 = set1 & extVar;   // As Integer has 32 bits space, we have to perform AND operation with the 
+                            // extVar and set1 to satisfy our 8 bits condition
 
-    int x = int(set1 | set2);
+    int set2 = n >> 4;
+    set2 = set2 & extVar;
+
+    int x = set1 ^ set2;
 
     cout << x;
 }
